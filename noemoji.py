b179f29df56a74a01786301bd99040980b11fa9f
Craxa <48805031+Craxa@users.noreply.github.com>
2022-12-13 19:37:57 +0000

    Create noemoji.py

---
+++ noemoji.py
@@ -0,0 +1,18 @@
+# ASCII alternatives
+
+world = 'o'
+python = '~~~-<'
+happy = ':-)'
+heart = '♡' # or '<3'
+star = '☆'
+sparkles = '✺'
+silly = ';)'
+sums = '+-*/'
+hundred = '100!'
+plead = '◔◔'
+fire = '/\\'
+books = '≣'
+rainbow = '⌒'
+dice = '⊡'
+clock = '◷'
+calendar = '▦'
